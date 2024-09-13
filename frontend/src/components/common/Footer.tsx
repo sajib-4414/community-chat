41f70fb9ed6f20b488e6091b87a98f194f0b9136
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2024-09-12 22:21:05 -0600

    triggerring frontned workflow

--- frontend/src/components/common/Footer.tsx
+++ frontend/src/components/common/Footer.tsx
@@ -8,7 +8,7 @@ const Footer:FC = ()=>{
         <a href="/" className="mb-3 me-2 mb-md-0  lh-1 text-decoration-none text-white">
           bbb
         </a>
-        <span className="mb-3 mb-md-0 text-body-secondary text-white">© 2024 Company, Inc</span>
+        <span className="mb-3  mb-md-0 text-body-secondary text-white">© 2024 Company, Inc</span>
       </div>
   
       <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
